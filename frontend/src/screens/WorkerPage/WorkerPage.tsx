340054aafa067c4d4e4cbe9c266055b3752b5836
danielcanatoy <danielicanatoy@iskolarngbayan.pup.edu.ph>
2026-01-20 23:43:56 +0800

    working worker profile

--- frontend/src/screens/WorkerPage/WorkerPage.tsx
+++ frontend/src/screens/WorkerPage/WorkerPage.tsx
@@ -4,7 +4,7 @@ import { WorkerHome } from "./sections/WorkerHome";
 import { WorkerProjects } from "./sections/WorkerProjects";
 import { WorkerMessages } from "./sections/WorkerMessages";
 import { WorkerProfile } from "./sections/WorkerProfile";
-import { WorkerListings } from "./sections/WorkerListings";
+import { WorkerListings } from "./sections/WorkerListing";
 
 import { useNavigate } from "react-router-dom";
 
@@ -102,17 +102,17 @@ export const WorkerPage = (): JSX.Element => {
 
         {/* Main Content */}
         <main className="flex-1 overflow-y-auto p-8">
-  {showProfileEdit ? (
-    <WorkerProfile onClose={() => setShowProfileEdit(false)} />
-  ) : (
-    <>
-      {activeSection === "home" && <WorkerHome />}
-      {activeSection === "projects" && <WorkerProjects />}
-      {activeSection === "listings" && <WorkerListings />}
-      {activeSection === "messages" && <WorkerMessages />}
-    </>
-  )}
-</main>
+          {showProfileEdit ? (
+            <WorkerProfile onClose={() => setShowProfileEdit(false)} />
+          ) : (
+            <>
+              {activeSection === "home" && <WorkerHome />}
+              {activeSection === "projects" && <WorkerProjects />}
+              {activeSection === "listings" && <WorkerListings />}
+              {activeSection === "messages" && <WorkerMessages />}
+            </>
+          )}
+        </main>
       </div>
     </div>
   );
