2af42fcccc8432bf2d706327c2f42d28829cba21
danielcanatoy <danielicanatoy@iskolarngbayan.pup.edu.ph>
2026-06-21 13:12:36 +0800

    Use environment variables for database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,17 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", // change if needed
-  password: "admin", // change if needed
-  database: "kokonut",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err) => {
