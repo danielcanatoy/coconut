7c1092c5ab003efbe6ae9f7edacf76db4ceb94b2
Shibadesu <talaniajanngabriel@gmail.com>
2026-06-22 20:58:16 +0800

    AAAAAAAAAAAAA

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,11 @@
 import mysql from "mysql2";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 db.connect((err) => {
